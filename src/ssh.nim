a8ca240ca9708df09420f90d2d1f1f8b1318cf86
treeform <starplant@gmail.com>
2018-10-06 10:59:50 -0700

    Updates and some more `os` style methods added

--- src/ssh.nim
+++ src/ssh.nim
@@ -57,7 +57,7 @@ proc command*(ssh: var SSH, command: string): string =
   ssh.commandNoOutput(command)
   var output = ssh.output()
   # remove the command itself
-  return output[command.len+1..^1]
+  return output[0..^1]
 
 
 proc writeFile*(ssh: var SSH, filepath: string, data: string) =
@@ -70,6 +70,7 @@ proc readFile*(ssh: var SSH, filepath: string): string =
   ssh.command("cat " & filepath)[0..^2].replace("\\n", "\n")
 
 
+
 proc setEnv*(ssh: var SSH, name: string, value: string) =
   # Sets environment variable, does not persist, only for this session
   if name == "PS1":
@@ -79,7 +80,40 @@ proc setEnv*(ssh: var SSH, name: string, value: string) =
 
 proc getEnv*(ssh: var SSH, name: string): string =
   # Reads environment variable
-  ssh.command("printenv " & name)
+  ssh.command("printenv " & name).strip()
+
+
+proc existsFile*(ssh: var SSH, filepath: string): bool =
+  # Warning, uses `cat`, to be used for small text files only
+  let
+    output = ssh.command("ls -l " & quoteShellPosix(filepath))
+    split = output.split("\n")
+  if split.len == 0:
+    return false
+  let
+    lsLine =split[0].splitWhitespace()
+  if lsLine.len < 8:
+    return false
+  lsLine[0][0] == '-' and lsLine[8] == filepath
+
+
+proc existsDir*(ssh: var SSH, filepath: string): bool =
+  # Warning, uses `cat`, to be used for small text files only
+  let
+    output = ssh.command("ls -l " & quoteShellPosix(filepath))
+  let
+    lsLine = output.split("\n")[0].splitWhitespace()
+  lsLine[0] == "total"
+
+
+proc getCurrentDir*(ssh: var SSH): string =
+  ssh.getEnv("PWD")
+
+
+proc setCurrentDir*(ssh: var SSH, path: string) =
+  discard ssh.command("cd " & quoteShellPosix(path))
+
+
 
 
 proc exit*(ssh: var SSH) =
@@ -91,14 +125,12 @@ proc exit*(ssh: var SSH) =
 
 proc newSSH*(serverAddress: string): SSH =
   # Start new SSH connection
-  result = SSH()
-  result.process = startProcess(openSSHPath, args=[serverAddress, "bash", "-i"])
+  var ssh = SSH()
+  ssh.process = startProcess(openSSHPath, args=[serverAddress, "sh", "-i"])
 
   # set the promt to some thing known so that we can split by it
-  result.commandNoOutput("export PS1=\"" & cmdSep.replace("\n", "\\n") & "\"")
-  # setting prompt produces 2 chunks, discard them
-  discard result.output()
-  discard result.output()
+  ssh.setEnv("PS1", cmdSep)
+  return ssh
 
 
 
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,8 +1,9 @@
-import os
+import os, json
 
 import ssh
 
-var server = newSSH("user@server") # make sure you have ssh keys setup, password not supported
+var server = newSSH("root@nyt.istrolid.com")
+#var server = newSSH("user@server") # make sure you have ssh keys setup, password not supported
 
 # run any simple, blocks until they are done
 echo server.command("uptime")
@@ -16,5 +17,15 @@ echo server.readFile("test.txt")
 # set and get environment variable, only sets it for this session
 server.setEnv("TERM", "xterm")
 echo server.getEnv("TERM")
+
+# some system commands in the style of the `os` module
+echo "existsFile:", server.existsFile("test.txt")
+echo "existsFile:", server.existsFile("test_other.txt")
+echo "existsDir:", server.existsDir("/var/log")
+echo "getCurrentDir:", server.getCurrentDir()
+server.setCurrentDir("/var/log")
+echo "getCurrentDir:", server.getCurrentDir()
+
+
 
 server.exit()
